f04a7e644e2ff1c813fb6936f6ee82f3d52a4a31
Adrien Joly <adrien.joly@gmail.com>
2017-10-17 14:30:51 +0200

    lighter render()

--- calendar-renderer/src/Calendar.js
+++ calendar-renderer/src/Calendar.js
@@ -8,6 +8,7 @@ const CONTAINER_HEIGHT = 700; // not including padding
 const CONTAINER_PADDING = 10; // in px
 
 // computed constants
+
 const HOURS_PER_DAY = DAY_END - DAY_START;
 const PIXELS_PER_MINUTE = CONTAINER_HEIGHT / HOURS_PER_DAY;
 
@@ -16,11 +17,22 @@ const containerStyle = {
   height: `${CONTAINER_HEIGHT + 2 * CONTAINER_PADDING}px`,
 };
 
+const styleLaidEvent = event => ({
+  height: `${(event.end - event.start) * PIXELS_PER_MINUTE}px`,
+  top: `${event.start * PIXELS_PER_MINUTE}px`,
+  left: CONTAINER_WIDTH * (event.pos / event.len),
+  width: CONTAINER_WIDTH / event.len,
+});
+
+// generic helpers
+
 const createArrayOfArrays = length =>
   Array.apply(null, { length }).map(() => []);
 
 const createRange = (first, last) =>
   Array.apply(null, { length: 1 + last - first }).map((v, i) => first + i);
+
+// layout helpers
 
 const computeMaxLen = (event, eventsPerHour) =>
   Math.max.apply(Math, event.slots.map(hour => eventsPerHour[hour].length));
@@ -51,20 +63,17 @@ function layoutEvents(events) {
   }));
 }
 
+// Calendar component
+
 class Calendar extends Component {
   render() {
     console.log(this.props);
     const laidEvents = layoutEvents(this.props.events);
     console.log('laid => ', laidEvents);
-    const eventStyle = laidEvents.map((event, index) => ({
-      height: `${(event.end - event.start) * PIXELS_PER_MINUTE}px`,
-      top: `${event.start * PIXELS_PER_MINUTE}px`,
-      left: CONTAINER_WIDTH * (event.pos / event.len),
-      width: CONTAINER_WIDTH / event.len,
-      // TODO: adjust left and width based on collisions
-    }));
-    const renderedEvents = this.props.events.map((event, index) =>
-      <div key={index} className="Calendar-event" style={eventStyle[index]}>
+    const styledEvents = laidEvents.map(event =>
+      Object.assign({}, event, { style: styleLaidEvent(event) }));
+    const renderedEvents = styledEvents.map((event, index) =>
+      <div key={index} className="Calendar-event" style={event.style}>
         <div className="Calendar-event-name">Sample Item ({event.start})</div>
         <div className="Calendar-event-location">Sample Location</div>
       </div>
