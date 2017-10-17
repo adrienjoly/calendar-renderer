75a8c50974360bb07a203a49fe35748d607bcddb
Adrien Joly <adrien.joly@gmail.com>
2017-10-17 10:48:14 +0200

    layoutDay()

--- calendar-renderer/src/App.js
+++ calendar-renderer/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     this.state = {
       events: []
     };
+    window.layoutDay = (events) => this.setState({ events });
   }
   render() {
     return (
@@ -18,5 +19,14 @@ class App extends Component {
     );
   }
 }
+
+setTimeout(() => {
+  window.layoutDay([
+    {start: 30, end: 150},
+    {start: 540, end: 600},
+    {start: 560, end: 620},
+    {start: 610, end: 670}
+  ]);
+}, 2000);
 
 export default App;
